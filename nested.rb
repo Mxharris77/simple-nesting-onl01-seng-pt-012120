352bf18780a49f4abf2ec23a5ed0c1041c5c35fa
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-04 04:29:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,12 +61,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-  programmer_hash = {
-    :yukihiro_matsumoto => {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-    }
-
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -82,8 +77,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-programmer_hash    
+programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages:
+      ["LISP", "C"]
+    }
+    
 end
 
 def changing_alan
